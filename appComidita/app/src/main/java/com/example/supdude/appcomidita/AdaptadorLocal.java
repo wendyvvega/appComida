0e2a2b563e69e91028c6beedaa755ae4816f1008
MixxaVega <wendyv.vega@gmail.com>
2018-05-16 01:58:41 -0600

    lista de locales

--- appComidita/app/src/main/java/com/example/supdude/appcomidita/AdaptadorLocal.java
+++ appComidita/app/src/main/java/com/example/supdude/appcomidita/AdaptadorLocal.java
@@ -1,6 +1,9 @@
 package com.example.supdude.appcomidita;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +12,76 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class AdaptadorLocal extends BaseAdapter {
+public class AdaptadorLocal extends RecyclerView.Adapter<AdaptadorLocal.PaletteViewHolder> {
+    private ArrayList<datosLocal> data;
+    private RecyclerViewOnItemClickListener recyclerViewOnItemClickListener;
+
+    public AdaptadorLocal(@NonNull ArrayList<datosLocal> data,
+                          @NonNull RecyclerViewOnItemClickListener
+                                          recyclerViewOnItemClickListener) {
+        this.data = data;
+        this.recyclerViewOnItemClickListener = recyclerViewOnItemClickListener;
+    }
+
+    @Override
+    public PaletteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.distribuyelocales, parent, false);
+        return new PaletteViewHolder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(PaletteViewHolder holder, int position) {
+        datosLocal local = data.get(position);
+        holder.getTitleTextView().setText(local.getNombreLocal());
+        holder.getSubtitleTextView().setText(local.getDireccion());
+
+        /*GradientDrawable gradientDrawable = (GradientDrawable) holder.getCircleView().getBackground();
+        int colorId = android.graphics.Color.parseColor(local.getHex());
+        gradientDrawable.setColor(colorId);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class PaletteViewHolder extends RecyclerView.ViewHolder implements View
+            .OnClickListener {
+        private ImageView circleView;
+        private TextView titleTextView;
+        private TextView subtitleTextView;
+
+
+        public PaletteViewHolder(View itemView) {
+            super(itemView);
+            circleView = itemView.findViewById(R.id.imgLocal);
+            titleTextView = (TextView) itemView.findViewById(R.id.txtNombreLocal);
+            subtitleTextView = (TextView) itemView.findViewById(R.id.txtDireccion);
+            itemView.setOnClickListener(this);
+        }
+
+        public TextView getTitleTextView() {
+            return titleTextView;
+        }
+
+        public TextView getSubtitleTextView() {
+            return subtitleTextView;
+        }
+
+        public View getCircleView() {
+            return circleView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            recyclerViewOnItemClickListener.onClick(v, getAdapterPosition());
+        }
+    }
+
+
+    /*
     Context contexto;
 
     public AdaptadorLocal(Context contexto, ArrayList<datosLocal> listaLocales) {
@@ -52,7 +123,7 @@ public class AdaptadorLocal extends BaseAdapter {
 
         tvNombre.setText(listaLocales.get(position).getNombreLocal());
        /* tvTelefono.setText(listaLocales.get(position).getTelefono());
-        tvEmail.setText(listaLocales.get(position).get());*/
+        tvEmail.setText(listaLocales.get(position).get());
         return vista;
-    }
+    }*/
 }
--- appComidita/app/src/main/java/com/example/supdude/appcomidita/listaLocales.java
+++ appComidita/app/src/main/java/com/example/supdude/appcomidita/listaLocales.java
@@ -2,26 +2,50 @@ package com.example.supdude.appcomidita;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class listaLocales extends AppCompatActivity {
 
-    ListView lista;
-    ArrayList<datosLocal> listaDeLocales;
+    ArrayList<datosLocal> lista;
+    //ArrayList<datosLocal> listaDeLocales;
 
     String opcion="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_locales);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        lista = findViewById(R.id.listaLocales);
-        listaDeLocales = new ArrayList<>();
+       // listaDeLocales = new ArrayList<>();
 
         Bundle  datos = getIntent().getExtras();
         opcion = datos.getString("tipoComida");
+
+        lista= new ArrayList<datosLocal>();
+        llenar(lista);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new AdaptadorLocal(lista, new RecyclerViewOnItemClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Toast toast = Toast.makeText(listaLocales.this, String.valueOf(position), Toast.LENGTH_SHORT);
+                int color = android.graphics.Color.parseColor(lista.get(position).getNombreLocal());
+                toast.getView().setBackgroundColor(color);
+                toast.show();
+            }
+        }));
+        //VERTICAL
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
         //llenarContactos(listaDeContactos);
 
         /*ArrayAdapter<contacto> adaptador = new ArrayAdapter<contacto>(this, R.layout.distribuyevistas,listaDeContactos);
@@ -29,5 +53,12 @@ public class listaLocales extends AppCompatActivity {
 
         /*adaptador = new AdaptadorLocal(this, listaDeLocales);
         lista.setAdapter(adaptador);*/
+    }
+
+    private void llenar(ArrayList lista) {
+
+        lista.add(new datosLocal("Local1", "uno@gmail.com", "Enrique segoviano","111111"));
+        lista.add(new datosLocal("Local2", "uno@gmail.com", "Enrique segoviano2","111111"));
+        lista.add(new datosLocal("Local3", "uno@gmail.com", "Enrique segoviano3","111111"));
     }
 }
